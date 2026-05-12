d9499769a355776a2d116f98f2c1094aa4d1f87b
TheXaruman <141373700+TheXaruman@users.noreply.github.com>
2026-05-12 16:18:32 +0200

    Added forward function

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
-use std::f64::consts::E;
 use ndarray::{Array1};
 use ndarray_rand::{RandomExt, rand_distr::{Uniform}};
-use rand;
 
-struct Neuron {
+pub struct Neuron {
     weights: Array1<f64>,
     bias: f64
 }
 
 impl Neuron {
-    fn construct() -> Self {
-        let n_inputs = 3;
+    pub fn construct(n_inputs: usize) -> Self {
         Neuron { 
             weights: (Array1::random(n_inputs, Uniform::new(0.0, 1.0).unwrap())), 
             bias: (rand::random_range(-1.0..1.0))
         }
     }
-    
+}
+
+pub fn forward(neuron: &Neuron, input: Array1<f64>) -> f64 {
+    let scalar_sum = input.dot(&neuron.weights);
+    let z = scalar_sum + neuron.bias;
+    1.0 / (1.0 + (-z).exp())
 }
