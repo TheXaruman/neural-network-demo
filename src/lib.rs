5216d7c3ef427a1a0ef1842c7a36ff3bcb922689
TheXaruman <141373700+TheXaruman@users.noreply.github.com>
2026-05-13 08:44:55 +0200

    Added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,38 @@ impl Neuron {
     }
 }
 
-pub fn forward(neuron: &Neuron, input: Array1<f64>) -> f64 {
+pub fn forward(neuron: &Neuron, input: &Array1<f64>) -> f64 {
+    assert_eq!(neuron.weights.len(), input.len(), "Vectors need to have the same length for dot product");
     let scalar_sum = input.dot(&neuron.weights);
     let z = scalar_sum + neuron.bias;
     1.0 / (1.0 + (-z).exp())
+}
+
+#[cfg(test)]
+mod test{
+    use ndarray::array;
+    use super::*;
+    #[test]
+    fn construct_random_weights() {
+        let neuron_1 = Neuron::construct(3);
+        let neuron_2 = Neuron::construct(3);
+
+        assert_ne!(neuron_1.weights, neuron_2.weights)
+    }
+
+    #[test]
+    fn construct_random_bias() {
+        let neuron_1 = Neuron::construct(3);
+        let neuron_2 = Neuron::construct(3);
+
+        assert_ne!(neuron_1.bias, neuron_2.bias)
+    }
+
+    #[test]
+    #[should_panic(expected = "Vectors need to have the same length for dot product")]
+    fn vector_length_difference_panic() {
+        let neuron = Neuron::construct(5);
+        let inputs = array!(1.0, 3.0);
+        forward(&neuron, &inputs);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use ndarray::array;
 fn main() {
     let neuron = Neuron::construct(3);
     let input = array![1.0, 2.0, 3.0];
-    let result = forward(&neuron, input);
+    let result = forward(&neuron, &input);
     println!("{}", result);
 }
