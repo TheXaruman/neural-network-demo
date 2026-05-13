5c09cb461ecc0786e16e002022692cf702133c7d
TheXaruman <141373700+TheXaruman@users.noreply.github.com>
2026-05-13 14:43:07 +0200

    Added Decay for Learning rate

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,19 @@ fn main() {
         [0.0, 0.0, 0.0],
     ];
     let mut neuron = Neuron::construct(2);
-    let mut loss = 0.0;
-    for _ in 1..10 {
-        for _ in 1..1000 {
+    for _ in 1..100 {
+        let mut loss = 0.0;
+        for _ in 1..100 {
             let training = rand::random_range(0..4) as usize;
             let input_slice = set.slice(s![training, 0..2]).to_owned();
             let target = set[[training, 2]];
             let output = forward(&neuron, &input_slice);
             let error_signal: f64 = output - target;
             neuron.train(output, input_slice, error_signal);
-            loss = (loss + error_signal) / 2.0;
+
+            loss = loss + error_signal.powi(2);
         }
-        println!("{}", loss);
+        Neuron::decay_learning_rate(0.99);
+        println!("{}", loss / 100.0);
     }
 }
